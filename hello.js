71cedb7a11cba883764c521a5a66d77573dbd4b2
suobing <suobing.sb@alibaba-inc.com>
2025-07-26 17:06:18 +0800

    export和require用法

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
 'use strict';
 
+console.log("Hello World!");
+
 const s = 'Hello';
 
 function greet(name) {
@@ -10,4 +12,6 @@ function bye(name) {
     console.log('bye' + ', ' + name + '!');
 }
 
+// 导出模块
+// * 默认情况下该文件hello.js就是一个模块hello，下面是为了可以使用hello.greet这种语法
 module.exports = {greet: greet, bye: bye};
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 // 引入hello模块:
-const hello = require('./hello');
+// * node和js的一大区别所在，js不支持require
+const hello = require('./hello'); // 需是相对路径，如果是const greet = require('hello'); 会在内置 全局 当前模块下找，会报错
 
 let s = 'Michael';
 
+// 因为export这里才可以用
 hello.greet(s); // Hello, Michael!
 hello.bye(s);
